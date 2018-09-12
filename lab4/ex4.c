5c5b06503408e1da382c25bf1d0892a5bd72d0d2
Exctues <alexnamecode@gmail.com>
2018-09-12 23:12:38 +0300

    End

--- lab4/ex4.c
+++ lab4/ex4.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-int  main(){
-	char str[255];
-	while(1){
-		printf(">>> ");
-		fgets(str, sizeof(str), stdin);
-		if(str[0] == 'e'  && str[1] == 'x' && str[2] == 'i' && str[3] == 't'){
-			break;					
-		}
-		int pid = fork();
-		if(pid == 0){
-			system(str);
-			break;
-		}
-	}
-	return 0;
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    char str[255];
+    while (1) {
+        printf(">>> ");
+        fgets(str, sizeof(str), stdin);
+        if (str[0] == 'e' && str[1] == 'x' && str[2] == 'i' && str[3] == 't') {
+            break;
+        }
+        int pid = fork();
+        if (pid == 0) {
+            system(str);
+            break;
+        }
+    }
+    return 0;
 }
